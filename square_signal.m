8c9a0993b4159a2b88622ae22cf9455b8ea38b9e
Nico <nico.encina2012@gmail.com>
2016-11-16 14:25:23 -0300

    Función square signal

--- square_signal.m
+++ square_signal.m
@@ -1,3 +1,21 @@
-function [time, signal] = square_signal()
+function [time, signal] = square_signal(frec, vpp, time, points)
+    period = 1/frec;
+    delta_T = time/(points-1);
+    time = linspace(0, time, points);
     
-end
+    time_counter = 0;
+    voltaje_actual = vpp/2;
+    
+    for i=1:points
+        signal(i) = voltaje_actual;
+        if time_counter > (period/2)
+            time_counter = 0;
+            if voltaje_actual == vpp/2
+                voltaje_actual = -vpp/2;
+            else
+                voltaje_actual = vpp/2;
+            end
+        end
+        time_counter = time_counter + delta_T;
+    end
+end 
